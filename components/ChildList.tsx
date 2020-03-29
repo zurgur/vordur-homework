c6006e39542346c431dd4f6b87b95123a302b13c
unknown <harryfreyr@gmail.com>
2020-03-29 16:06:44 +0000

    add styling to list

--- components/ChildList.tsx
+++ components/ChildList.tsx
@@ -1,7 +1,17 @@
 import List from '@material-ui/core/List';
+import { makeStyles } from '@material-ui/core/styles';
+
 import ChildCard from './ChildCard';
 
+const useStyles = makeStyles(() => ({
+  list: {
+    display: 'flex',
+    flexFlow: 'wrap',
+  },
+}));
+
 export default (): JSX.Element => {
+  const classes = useStyles();
   const kids = [
     { name: 'peter pan', kt: '111111 - 1111' },
     { name: 'Tootles ', kt: '111111 - 1111' },
@@ -9,7 +19,7 @@ export default (): JSX.Element => {
     { name: 'Slightly ', kt: '111111 - 1111' },
   ];
   return (
-    <List dense>
+    <List dense className={classes.list}>
       {kids.map((lostBoy): JSX.Element => (
         <ChildCard
           key={lostBoy.name}
