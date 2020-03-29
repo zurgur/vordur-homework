b3b169a1a81f237b5b405f8bdca7c2f91f2d88c1
unknown <harryfreyr@gmail.com>
2020-03-29 15:28:45 +0000

    add prize select

--- components/ChildCard.tsx
+++ components/ChildCard.tsx
@@ -4,6 +4,14 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 import Typography from '@material-ui/core/Typography';
 import Switch from '@material-ui/core/Switch';
 import Paper from '@material-ui/core/Paper';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Checkbox from '@material-ui/core/Checkbox';
 
 // #region styles
 export const ValitorColor = '#FCBD12';
@@ -69,6 +77,10 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     alignItems: 'center',
   },
+  list: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
 }));
 
 // #endregion
@@ -81,6 +93,8 @@ interface Props {
 export default ({ name, kt }: Props): JSX.Element => {
   const [open, setOpen] = useState(false);
   const classes = useStyles();
+  const [ priceIndex, setPriceIndex ] = useState(0);
+  const prizes = ['25.000.000', '30.000.000', '35.000.000'];
   return (
     <Paper className={classes.paper}>
       <img src="./childIcon-1.svg" alt="child" className={classes.headImage} />
@@ -99,26 +113,49 @@ export default ({ name, kt }: Props): JSX.Element => {
         </Typography>
       </div>
       <hr className={classes.hr} />
-      <div className={classes.container}>
-        <div className={classes.price}>
-          <p>bótafarhæð</p>
-          <h3 className={classes.nameStyle}>35.000.000 kr.</h3>
-          <button type="button" onClick={(): void => setOpen(!open)} className={classes.button}>
-            <ArrowForwardIosIcon style={{ ...arrowIconStyle, transform: `rotate(${Number(open) * 90}deg)` }} />
-            Breyta
-          </button>
-        </div>
-        <hr className={classes.hr} />
-        <div className={classes.price}>
-          <p>tegund</p>
-          <h3 className={classes.nameStyle}>Barnatrygging 1</h3>
-          <button type="button" className={classes.button}>
-            <ArrowForwardIosIcon style={arrowIconStyle} />
-            Nánar
-          </button>
-        </div>
-      </div>
-      <hr className={classes.hr} />
+      <ExpansionPanel expanded={open}>
+        <ExpansionPanelSummary className={classes.container}>
+          <div className={classes.price}>
+            <p>bótafarhæð</p>
+            <h3 className={classes.nameStyle}>35.000.000 kr.</h3>
+            <button type="button" onClick={(): void => setOpen(!open)} className={classes.button}>
+              <ArrowForwardIosIcon style={{ ...arrowIconStyle, transform: `rotate(${Number(open) * 90}deg)` }} />
+              Breyta
+            </button>
+          </div>
+          <hr className={classes.hr} />
+          <div className={classes.price}>
+            <p>tegund</p>
+            <h3 className={classes.nameStyle}>Barnatrygging 1</h3>
+            <button type="button" className={classes.button}>
+              <ArrowForwardIosIcon style={arrowIconStyle} />
+              Nánar
+            </button>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.list}>
+          <Typography variant="h6">
+            Veldu upphæð:
+          </Typography>
+          <List dense>
+            {prizes.map((prize, index): JSX.Element => (
+              <ListItem button key={prize}>
+                <ListItemIcon>
+                  <Checkbox
+                    edge="start"
+                    checked={priceIndex === index}
+                    onClick={(): void => setPriceIndex(index)}
+                    tabIndex={-1}
+                    disableRipple
+                    style={{ color: ValitorColor }}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={prize} />
+              </ListItem>
+            ))}
+          </List>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
       <div className={classes.priceStyle}>
         <Typography>
           iðgjöld
